25f1ac28a7ba6c15565a339be5ee39a80c9e05bf
Hamza Hussain <73032972+hamzah-developer@users.noreply.github.com>
2023-03-04 21:08:50 +0000

    working through the calculator component

---
+++ resources/js/app.js
@@ -0,0 +1,41 @@
+const calculator = document.querySelector('.calculator')
+const keys = document.querySelector('.calculator_keys')
+const display = document.querySelector('.calculator_display')
+
+keys.addEventListener('click', e => {
+  if (e.target.matches('button')) {
+   const key = e.target
+   const action = key.dataset.action
+   const displayedNum = display.textContent
+   const keyContent = key.textContent
+
+   if(!action) {
+    if (displayedNum === '0') {
+      display.textContent = keyContent
+    } else {
+      display.textContent = displayedNum + keyContent
+    }
+   } 
+
+   if (action === 'decimal') {
+    display.textContent = displayedNum + '.'
+   }
+
+   if (
+    action === 'add' ||
+     action === 'subtract' 
+     || action === 'multiply' 
+     || action === 'divide' 
+     || action === 'decimal' 
+      || action === 'clear' 
+      || action === 'calculate'
+   ) {
+    key.classList.add('is-depressed')
+   }
+
+
+
+
+
+  }
+ })
